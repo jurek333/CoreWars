6eadc2aa3768fc63165f2b4bd919a0c02b67bd7e
jurek333 <jurek.333@gmail.com>
2016-03-16 00:38:37 +0100

    change .NET Framework version to 4.6.1, add heartbeat controller to be able said if still all are alive, add new project to implement logging

---
+++ CoreWars.WebApi/Diagnostic/AppHeartbeat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreWars.WebApi.Diagnostic
+{
+    public class Heartbeat
+    {
+        public Heartbeat()
+        {
+            this.Time = DateTime.UtcNow;
+        }
+        public Heartbeat(string desc):this()
+        {
+            this.Description = desc;
+        }
+        public DateTime Time { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class AppHeartbeat: Heartbeat
+    {
+        public AppHeartbeat():base() {
+            this.AnotherModules = new List<AppHeartbeat>();
+        }
+
+        public AppHeartbeat(string desc) : base(desc)
+        {
+            this.AnotherModules = new List<AppHeartbeat>();
+        }
+
+        public List<AppHeartbeat> AnotherModules { get; set; }
+    }
+}
---
+++ CoreWars.WebApi/Diagnostic/HeartbeatController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CoreWars.WebApi.Diagnostic
+{
+    public class HeartbeatController : ApiController
+    {
+        public AppHeartbeat Get()
+        {
+            return new AppHeartbeat("Heartbeat controller is alive.");
+        }
+    }
+}
